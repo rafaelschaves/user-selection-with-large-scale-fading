5c1fb1c860ac1768935b80f3ec8d888d9f9f6258
Rafael Chaves <rflschaves@poli.ufrj.br>
2021-05-21 22:29:44 -0300

    Change parameter for simulation

--- user_selection_ur_los_large_scale.m
+++ user_selection_ur_los_large_scale.m
@@ -29,7 +29,7 @@ N_PA  = 2;
 
 commcell.nAntennas       = M;                                        % Number of Antennas
 commcell.nUsers          = K;                                        % Number of Users
-commcell.radius          = 500;                                      % Cell's raidus (circumradius) in meters
+commcell.radius          = 2000;                                      % Cell's raidus (circumradius) in meters
 commcell.bsHeight        = 32;                                       % Height of base station in meters
 commcell.userHeight      = [1 2];                                    % Height of user terminals in meters ([min max])
 commcell.nPaths          = 30;                                       % Number of Multipaths
