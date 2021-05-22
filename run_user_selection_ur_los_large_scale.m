68e06a6566336f81ad569247ccad73da12d76329
Rafael Chaves <rflschaves@poli.ufrj.br>
2021-05-21 22:16:52 -0300

    Change parameter for simulation

--- run_user_selection_ur_los_large_scale.m
+++ run_user_selection_ur_los_large_scale.m
@@ -9,9 +9,9 @@ warning('off','MATLAB:singularMatrix');
 
 MC = 1000;                                                              % Size of the outer Monte Carlo ensemble (Varies the channel realizarions)
 
-M = 100;                                                                % Number of antennas at the base station
+M = 50;                                                                % Number of antennas at the base station
 
-for K = [10 25 50 75 100 150]                                           % Number of users at the cell
+for K = [10 25 50 75]                                           % Number of users at the cell
     K
     run user_selection_ur_los_large_scale.m
 end
